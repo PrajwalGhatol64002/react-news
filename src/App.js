6baf9ef9c6296f63fa886ba0a09a78c7eff545b8
Prajwal Ghatol <prajwalghatol00@gmail.com>
2023-12-19 19:46:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import News from './Components/News';
+
+function App() {
+  return (
+    <div className='bg-slate-50 dark:bg-slate-900'>
+      <div className='container mx-auto'>
+        <News />
+      </div>
+    </div>
+  );
+}
+
+export default App;
